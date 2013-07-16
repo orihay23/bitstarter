96048a863f2f5770a1c6fbd10829b2cec98b47fb
Ubuntu <ubuntu@ip-172-31-13-251.us-west-2.compute.internal>
2013-07-16 20:44:31 +0000

    Updated to be accessible over 8080

--- web.js
+++ web.js
@@ -2,13 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var index = fs.readFileSync('index.html').toString('utf8');
+var htmlfile = "index.html";
 
 app.get('/', function(request, response) {
-  response.send(index);
+    var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
